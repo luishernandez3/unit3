4996d60f4bacd2a2d5feea9af65a742fcb167643
luishernandez3 <123015178+luishernandez3@users.noreply.github.com>
2023-02-26 19:36:50 -0600

    Update nameSort.kt

--- nameSort.kt
+++ nameSort.kt
@@ -7,13 +7,10 @@ fun main() {
     print("Enter the third name: ")
     val name3 = readLine()!!
 
-    // Create a list containing the three names
     val names = listOf(name1, name2, name3)
 
-    // Sort the names in ascending order
     val sortedNames = names.sorted()
 
-    // Display the sorted names
     println("The names in ascending order are:")
     for (name in sortedNames) {
         println(name)
